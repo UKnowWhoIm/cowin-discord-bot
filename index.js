b45c7c04da0656cadf4568b4ac4ee1067b8486e5
irene <irenekurien01@gmail.com>
2021-05-20 01:50:43 +0530

    lint fix

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ config();
 
 const url = process.env.MONGODB_SRV;
 
-try {
-    await mongoose.connect(url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-    });
-} catch (error) {
-    console.log(error);
+async function start() {
+    try {
+        await mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
+
+start();
