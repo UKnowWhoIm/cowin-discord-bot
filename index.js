7d4459c52282f4ba9964a85a4a4a21b223aafe14
Sidharth <sidharthajithkumar@gmail.com>
2021-05-22 00:50:23 +0530

    added procfile

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+import express from 'express';
 import mongoose from "mongoose";
 import { job } from "./cron.js";
 import cron from "node-cron";   
 import { DEBUG, mongoURL } from "./config.js";
+
+const app = express();
+const port = process.env.PORT ?? "3000";
 
 async function start() {
     try {
@@ -18,6 +22,14 @@ async function start() {
 start();
 if(DEBUG)
     console.log("DEBUG Mode");
+
+app.get('/', (_, res) => {
+    res.send('Hello World!')
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
 
 // In debug mode send notif every minute
 cron.schedule(DEBUG ? "* * * * *" : "0 * * * *", () => {
