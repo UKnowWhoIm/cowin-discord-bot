72c8c9da791bf188359c17adf9adb8d9ee3bcf34
Alexandr Zawgorodniy <alexandr.zawgorodniy@gmail.com>
2021-06-11 16:22:25 +0700

    add age slot to getAge

--- src/commands/common.js
+++ src/commands/common.js
@@ -1,17 +1,17 @@
 import { findBestMatch } from "string-similarity";
 import { getDistricts } from "../dbCrud.js";
 
-export class Command{
+export class Command {
     static instances = []; // jshint ignore:line
 
-    constructor(cmdName, cmdData, cmdCallBack){
+    constructor(cmdName, cmdData, cmdCallBack) {
         this.name = cmdName;
         this.data = cmdData;
         this.callback = cmdCallBack;
         Command.instances.push(this);
     }
 
-    static initialize(callback){
+    static initialize(callback) {
         // Create all commands
         Command.instances.forEach(async (instance) => {
             await callback({
@@ -20,7 +20,7 @@ export class Command{
         });
     }
 
-    static getCallBackMap(){
+    static getCallBackMap() {
         let map = {};
         Command.instances.forEach((instance) => {
             map[instance.name] = instance.callback;
@@ -30,7 +30,7 @@ export class Command{
 }
 
 
-export function sendReply(bot, interaction, msg){
+export function sendReply(bot, interaction, msg) {
     bot.api.interactions(interaction.id, interaction.token).callback.post({
         data: {
             type: 4,
@@ -42,14 +42,23 @@ export function sendReply(bot, interaction, msg){
 }
 export const getUserID = (interaction) => interaction.member.user.id;
 
-export const getAge = (age) => age >= 45 ? 45 : 18;
+export const getAge = (age) => {
+    switch (true) {
+        case age >= 40 && age <= 44:
+            return 40;
+        case age >= 45:
+            return 45;
+        default:
+            return 18;
+    }
+}
 
-export async function sendDM(bot, userId, msg){
+export async function sendDM(bot, userId, msg) {
     const user = await bot.users.fetch(userId);
     user.send(msg);
 }
 
-export async function parseDistrict(district){
+export async function parseDistrict(district) {
     // TODO: return multiple matches if it exists.
     const minDiceCoeff = 0.8;
     const districtsInDb = await getDistricts();
@@ -59,13 +68,13 @@ export async function parseDistrict(district){
 
     return {
         "name": matches[0]?.target,
-        "code": districtsInDb.filter((district) => 
+        "code": districtsInDb.filter((district) =>
             district.districtName.toLowerCase() === matches[0]?.target
         )[0]?.districtID
     };
 }
 
-function getSessionText(sessions){
+function getSessionText(sessions) {
     let result = "";
     sessions.forEach((session) => result += `
 -------- DATE: ${session.date} --------
@@ -76,25 +85,25 @@ Dose2 Capacity: ${session.dose2Capacity}`);
     return result;
 }
 
-export async function processResults(bot, interaction, results, userID){
-    if(results.length > 0){
-        if(!userID)
+export async function processResults(bot, interaction, results, userID) {
+    if (results.length > 0) {
+        if (!userID)
             sendReply(bot, interaction,
                 "Slots available");
-        for(const center of results)
+        for (const center of results)
             await sendDM(bot, userID ?? getUserID(interaction), `
 <<<<<<<< New Center Available >>>>>>>>
 Center Name: ${center.name}
 Address: ${center.address}
 Pincode: ${center.pincode}
 Fee Type: ${center.feeType} ${getSessionText(center.sessions)}`);
-        
+
         return sendDM(bot, userID ?? getUserID(interaction),
-`Register Now: https://www.cowin.gov.in/home
+            `Register Now: https://www.cowin.gov.in/home
 ***I'm a bot, so don't reply***....
-`);            
-        }
-        if(!userID)
-            return sendReply(bot, interaction, 
-                "No slots available");
+`);
+    }
+    if (!userID)
+        return sendReply(bot, interaction,
+            "No slots available");
 }
